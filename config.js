0b94d9d4dada1971f4090d5bb331aa8c4ec174c5
tuan214 <117826313+tuan214@users.noreply.github.com>
2022-11-11 22:23:45 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Mẽo';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100028615097716';
+const eventName = 'Happy Birthday';
+const titleCard = 'Ngọc Mẽo';
+const contentCard = 'Chúc Mẽo có 1 ngày sinh nhật thật dui dẻ,hạnh phúc và nhứng điều may mắn nhất sẽ đến với m, đây là một món quà nho nhỏ do t tự làm nhee, hi vọng m sẽ thícc =))';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'Screenshot_20221111_063406.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
